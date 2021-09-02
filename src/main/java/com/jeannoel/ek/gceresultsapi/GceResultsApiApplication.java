be32b3741dca6be15374db04b314344d38f28c3a
Elroy Kanye <elroykanye@gmail.com>
2021-09-02 22:58:25 +0100

    ek-ac: added ./src/main/java/com/jeannoel/ek/gceresultsapi/GceResultsApiApplication.java

---
+++ src/main/java/com/jeannoel/ek/gceresultsapi/GceResultsApiApplication.java
@@ -0,0 +1,13 @@
+package com.jeannoel.ek.gceresultsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GceResultsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GceResultsApiApplication.class, args);
+	}
+
+}
