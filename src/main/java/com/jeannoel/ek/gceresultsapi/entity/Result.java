0580a4b91d17d4fa0f81448c710c709ba0b0eb0e
Elroy Kanye <elroykanye@gmail.com>
2021-09-03 13:09:52 +0100

    ek-autocomm: added ./src/main/java/com/jeannoel/ek/gceresultsapi/entity/Result.java

--- src/main/java/com/jeannoel/ek/gceresultsapi/entity/Result.java
+++ src/main/java/com/jeannoel/ek/gceresultsapi/entity/Result.java
@@ -1,11 +1,14 @@
 package com.jeannoel.ek.gceresultsapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -15,12 +18,20 @@ import javax.persistence.Id;
 @Entity
 public class Result {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @JsonProperty("center_name")
     private String centreName;
+    @JsonProperty("center_number")
     private String centreNumber;
+    @JsonProperty("level")
     private String level;
+    @JsonProperty("year")
     private String year;
+    @JsonProperty("papers_passed")
     private String papersPassed;
+    @JsonProperty("student_name")
     private String studentName;
+    @JsonProperty("student_grades")
     private String studentGrades;
 }
