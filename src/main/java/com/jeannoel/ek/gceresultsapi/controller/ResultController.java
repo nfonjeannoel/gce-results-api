e0f06abf850140d4eb1e0bc646c6a9ef6112bbb0
Elroy Kanye <elroykanye@gmail.com>
2021-09-03 13:09:52 +0100

    ek-autocomm: added ./src/main/java/com/jeannoel/ek/gceresultsapi/controller/ResultController.java

--- src/main/java/com/jeannoel/ek/gceresultsapi/controller/ResultController.java
+++ src/main/java/com/jeannoel/ek/gceresultsapi/controller/ResultController.java
@@ -1,2 +1,43 @@
-package com.jeannoel.ek.gceresultsapi.controller;public class ResultController {
+package com.jeannoel.ek.gceresultsapi.controller;
+
+import com.jeannoel.ek.gceresultsapi.entity.Result;
+import com.jeannoel.ek.gceresultsapi.service.ResultService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "result")
+public class ResultController {
+
+    private final ResultService resultService;
+
+    @Autowired
+    public ResultController(ResultService resultService) {
+        this.resultService = resultService;
+    }
+
+    /**
+     * Handles the request for getting all results collected in the database
+     * @return list of Result objects found
+     */
+    @GetMapping(value = "all")
+    public List<Result> getAll() {
+        return resultService.getAllResults();
+    }
+
+
+    @GetMapping(value = "/{centre_number}")
+    public List<Result> getAllByCentreNumber(@PathVariable(name = "centre_number") String centreNumber) {
+        return resultService.getAllResultsByCentreNumber(centreNumber);
+    }
+
+    @GetMapping(value = "/{centre_number}/{papers_passed}")
+    public List<Result> getAllByCentreNumberAndPapersPassed(
+            @PathVariable(name = "centre_number") String centreNumber,
+            @PathVariable(name = "papers_passed") String papersPassed
+    ) {
+        return resultService.getAllResultsByCentreNumberAndPapersPassed(centreNumber, papersPassed);
+    }
 }
