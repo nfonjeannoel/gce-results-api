430e95f8357fce5fff3809c813191ff96b6531eb
Elroy Kanye <elroykanye@gmail.com>
2021-09-03 13:09:51 +0100

    ek-autocomm: added ./src/main/java/com/jeannoel/ek/gceresultsapi/repository/ResultRepository.java

--- src/main/java/com/jeannoel/ek/gceresultsapi/repository/ResultRepository.java
+++ src/main/java/com/jeannoel/ek/gceresultsapi/repository/ResultRepository.java
@@ -1,2 +1,13 @@
-package com.jeannoel.ek.gceresultsapi.repository;public class ResultRepository {
+package com.jeannoel.ek.gceresultsapi.repository;
+
+import com.jeannoel.ek.gceresultsapi.entity.Result;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface ResultRepository extends JpaRepository<Result, Long> {
+    List<Result> findAllByCentreNumber(String centreNumber);
+
+    List<Result> findAllByCentreNumberAndPapersPassed(String centreNumber, String papersPassed);
 }
